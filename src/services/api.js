164ddbc7d9e4e1bf603d37381b1edc0a7cd13062
dhanush75cs <dhanush75dd@gmail.com>
2025-07-19 00:43:59 +0530

    Files upload

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "3a405b9223e3e122839884d376bc8d40";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
